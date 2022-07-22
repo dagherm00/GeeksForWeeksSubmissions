39933a7b776da91aa8da088670177f41f39878cf
malak <108842921+dagherm00@users.noreply.github.com>
2022-07-21 20:57:48 -0400

    Day 1 Submission

---
+++ day2.py
@@ -0,0 +1,37 @@
+a=2
+b=3
+print(a+b)
+x="Hello"
+u=" "
+y="World"
+print(x + u + y)
+print(x,y)
+d=1.5
+print(d)
+print(d+a+b)
+e=True
+f=False
+print(e)
+print(f)
+
+if(a+b<=4):
+    print("this is true")
+    
+if(f==False):
+    print(x,y)
+    e=False
+    f=True
+
+if(f==False):
+    e=False
+    f=True
+
+while(f==False):
+    print("i'm stoopid and you're ugly")
+
+work=[2,4,5,6,7,8,9,2,7,5,11]
+for x in work:
+    print(x+1)
+
+for x in range(10):
+    print("Hello")
